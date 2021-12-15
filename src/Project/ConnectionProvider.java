f4c87ce05d5463c86c769eeb84ac1c619203f2ba
Your Name <you@example.com>
2021-12-15 22:09:25 +0700

    first commit

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author Admin
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+}
